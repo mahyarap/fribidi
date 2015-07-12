d550bd797b903bb1001eb2fa0f767149fe471df0
Mahyar Abbas Pour <m.abbaspour@pishro.computer>
2015-07-12 15:15:02 +0430

    Add Doxygen support

---
+++ doc/html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['_5ffribidimemchunk',['_FriBidiMemChunk',['../struct__FriBidiMemChunk.html',1,'']]],
+  ['_5ffribidirunstruct',['_FriBidiRunStruct',['../struct__FriBidiRunStruct.html',1,'']]],
+  ['_5fpairmap',['_PairMap',['../struct__PairMap.html',1,'']]]
+];
---
+++ doc/html/search/all_1.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['fribidi_2dbidi_2dtypes_2ec',['fribidi-bidi-types.c',['../fribidi-bidi-types_8c.html',1,'']]],
+  ['fribidi_2dbidi_2ec',['fribidi-bidi.c',['../fribidi-bidi_8c.html',1,'']]],
+  ['fribidi_2dbidi_2eh',['fribidi-bidi.h',['../fribidi-bidi_8h.html',1,'']]],
+  ['fribidi_2ddeprecated_2ec',['fribidi-deprecated.c',['../fribidi-deprecated_8c.html',1,'']]],
+  ['fribidi_2ec',['fribidi.c',['../fribidi_8c.html',1,'']]],
+  ['fribidi_5fget_5fpar_5fdirection',['fribidi_get_par_direction',['../fribidi-bidi_8c.html#a36ce68baef8b058ddd6d30e689c25299',1,'fribidi_get_par_direction(const FriBidiCharType *bidi_types, const FriBidiStrIndex len):&#160;fribidi-bidi.c'],['../fribidi-bidi_8h.html#a36ce68baef8b058ddd6d30e689c25299',1,'fribidi_get_par_direction(const FriBidiCharType *bidi_types, const FriBidiStrIndex len):&#160;fribidi-bidi.c']]],
+  ['fribidi_5fget_5fpar_5fembedding_5flevels',['fribidi_get_par_embedding_levels',['../fribidi-bidi_8c.html#a6e365fb877091e74c8bbe8916e171baf',1,'fribidi_get_par_embedding_levels(const FriBidiCharType *bidi_types, const FriBidiStrIndex len, FriBidiParType *pbase_dir, FriBidiLevel *embedding_levels):&#160;fribidi-bidi.c'],['../fribidi-bidi_8h.html#a9df43cff100877238408249d6d52f1a8',1,'fribidi_get_par_embedding_levels(const FriBidiCharType *bidi_types, const FriBidiStrIndex len, FriBidiParType *pbase_dir, FriBidiLevel *embedding_levels) FRIBIDI_GNUC_WARN_UNUSED:&#160;fribidi-bidi.c']]],
+  ['fribidi_5freorder_5fline',['fribidi_reorder_line',['../fribidi-bidi_8c.html#a33f5e123ad260bd14b61b899b289bd90',1,'fribidi_reorder_line(FriBidiFlags flags, const FriBidiCharType *bidi_types, const FriBidiStrIndex len, const FriBidiStrIndex off, const FriBidiParType base_dir, FriBidiLevel *embedding_levels, FriBidiChar *visual_str, FriBidiStrIndex *map):&#160;fribidi-bidi.c'],['../fribidi-bidi_8h.html#a1fb49156015c32c7eb4e3f57252076ee',1,'fribidi_reorder_line(FriBidiFlags flags, const FriBidiCharType *bidi_types, const FriBidiStrIndex len, const FriBidiStrIndex off, const FriBidiParType base_dir, FriBidiLevel *embedding_levels, FriBidiChar *visual_str, FriBidiStrIndex *map) FRIBIDI_GNUC_WARN_UNUSED:&#160;fribidi-bidi.c']]]
+];
